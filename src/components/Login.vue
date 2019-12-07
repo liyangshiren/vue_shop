88cca313d67d119a91671dc2bde487ab0068851a
li <372902817@qq.com>
2019-12-07 19:55:18 +0800

    登陆拿到返回值了

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,21 +3,31 @@
     <div class="login_box">
       <!--头像区域-->
       <div class="avatar_box">
-        <img src="../assets/logo.png" alt="">
+        <img src="../assets/logo.png" alt>
       </div>
       <!--登陆表单-->
-      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" lable-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        lable-width="0px"
+        class="login_form"
+      >
         <!--用户名-->
         <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!--密码-->
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" type="password"></el-input>
+          <el-input
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+            type="password"
+          ></el-input>
         </el-form-item>
         <!--按钮区域-->
         <el-form-item class="btns">
-          <el-button type="primary">登陆</el-button>
+          <el-button type="primary" @click="login">登陆</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -53,58 +63,67 @@ export default {
     // 点击重置按钮，重置登陆表单
     resetLoginForm () {
       this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) {
+
+        } else {
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          console.log(res)
+        }
+      })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-  .login_container {
-    background-color: #2b4b6b;
-    height: 100%;
-  }
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
 
-  .login_box {
-    width: 450px;
-    height: 300px;
-    background-color: #fff;
-    border-radius: 3px;
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: #ddd;
     position: absolute;
     left: 50%;
-    top: 50%;
     transform: translate(-50%, -50%);
-
-    .avatar_box {
-      height: 130px;
-      width: 130px;
-      border: 1px solid #eee;
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
       border-radius: 50%;
-      padding: 10px;
-      box-shadow: #ddd;
-      position: absolute;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      background-color: #fff;
-      img {
-        width: 100%;
-        height: 100%;
-        border-radius: 50%;
-        background-color: #eee;
-      }
+      background-color: #eee;
     }
   }
+}
 
-  .login_form {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    box-sizing: border-box;
-  }
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
 
-  .btns {
-    display: flex;
-    justify-content: flex-end;
-  }
-
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,10 @@ import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 
+import axios from 'axios'
+Vue.prototype.$http = axios
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
