1d2134107fdbeb22d59fbeeb3c05e6f789f9f22d
li <372902817@qq.com>
2019-12-07 16:36:29 +0800

    绘制登陆盒子

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        登陆组件
-    </div>
+  <div class="login_container">
+    <div class="login_box"></div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +9,19 @@ export default {}
 </script>
 
 <style lang="less" scoped>
+  .login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+  }
 
+  .login_box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
 </style>
