ddb72cd55e808d7e25aaccce65f0285fbcf73240
li <372902817@qq.com>
2019-12-08 10:24:22 +0800

    获取左侧菜单数据

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import { Button, Form, FormItem, Input, Container, Header, Aside, Main,
-Menu, Submenu, MenuItemGroup, MenuItem} from 'element-ui'
+Menu, Submenu, MenuItem} from 'element-ui'
 //导入弹框提示组件
 import { Message } from 'element-ui'
 
@@ -14,7 +14,6 @@ Vue.use(Aside)
 Vue.use(Main)
 Vue.use(Submenu)
 Vue.use(Menu)
-Vue.use(MenuItemGroup)
 Vue.use(MenuItem)
 //挂载到Vue组件上，这样每个vue都可以直接通过this.$message来访问
 Vue.prototype.$message = Message
