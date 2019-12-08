3df8927eba77e92dc4f2e2b961debded9eb91ce4
li <372902817@qq.com>
2019-12-08 11:46:20 +0800

    用户列表基础布局

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <h3>用户列表组件</h3>
+    <!--面包屑导航区-->
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图区域-->
+    <el-card>
+      <!--搜索与添加区-->
+      <div style="margin-top: 15px;">
+        <el-row :gutter="20">
+          <el-col :span="9">
+            <el-input placeholder="请输入内容">
+              <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+          </el-col>
+          <el-col :span="4"></el-col>
+          <el-button type="primary">添加用户</el-button>
+        </el-row>
+      </div>
+    </el-card>
   </div>
 </template>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Container, Header, Aside, Main,
-Menu, Submenu, MenuItem} from 'element-ui'
+import {
+  Button, Form, FormItem, Input, Container, Header, Aside, Main,
+  Menu, Submenu, MenuItem,
+  Breadcrumb, BreadcrumbItem, Card, Row, Col
+} from 'element-ui'
 //导入弹框提示组件
 import { Message } from 'element-ui'
 
@@ -15,5 +18,10 @@ Vue.use(Main)
 Vue.use(Submenu)
 Vue.use(Menu)
 Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
 //挂载到Vue组件上，这样每个vue都可以直接通过this.$message来访问
 Vue.prototype.$message = Message
