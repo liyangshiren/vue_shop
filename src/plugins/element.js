3256e76e723438cd9273703ce6fba4355ee7ecaf
li <372902817@qq.com>
2019-12-08 10:05:08 +0800

    侧边栏菜单

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Container, Header, Aside, Main } from 'element-ui'
+import { Button, Form, FormItem, Input, Container, Header, Aside, Main,
+Menu, Submenu, MenuItemGroup, MenuItem} from 'element-ui'
 //导入弹框提示组件
 import { Message } from 'element-ui'
 
@@ -11,5 +12,9 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Submenu)
+Vue.use(Menu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
 //挂载到Vue组件上，这样每个vue都可以直接通过this.$message来访问
 Vue.prototype.$message = Message
