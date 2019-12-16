7de6038d3c15e7a6077aee6ea73c176b1b3edae2
li <372902817@qq.com>
2019-12-16 09:37:21 +0800

    获取商品分类数据

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -22,13 +22,36 @@
 
 <script>
   export default {
-    data(){
-      return{}
+    data() {
+      return {
+        //查询条件
+        queryInfo: {
+          type: 3,
+          pagenum: 1,
+          pagesize: 5
+        },
+        //商品分类的数据列表，默认为空
+        catelist: [],
+        //总数据条数
+        total:''
+      }
     },
     created() {
+      this.getCateList()
     },
-    methods(){
+    methods: {
+      //获取商品分类数据
+      async getCateList() {
+        const { data: res } = await this.$http.get(`categories`, { params: this.queryInfo })
 
+        if(res.meta.status !== 200){
+          return this.$message.error('获取商品分类失败！')
+        }
+        //把数据列表，赋值给 catelist
+        this.catelist = res.data.result
+        //为总数据条数赋值
+        this.total = res.data.total
+      }
     }
   }
 </script>
