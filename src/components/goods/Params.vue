324ea43e47a586b5ed6fc7b66f3a3b395c9b852a
li <372902817@qq.com>
2019-12-19 13:43:56 +0800

    动态参数和静态属性的添加操作

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -31,15 +31,60 @@
         <!--添加动态参数的面板-->
         <el-tab-pane label="动态参数" name="many">
           <!--添加动态参数的按钮-->
-          <el-button type="primary" size="mini" :disabled="isBtnDisable">添加参数</el-button>
+          <el-button type="primary" size="mini" :disabled="isBtnDisable" @click="addDialogVisible=true">添加参数</el-button>
+          <!--动态参数的表格-->
+          <el-table :data="manyTableData" border stripe>
+            <!--展开行-->
+            <el-table-column type="expand"></el-table-column>
+            <!--索引列-->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
         <!--添加静态属性的面板-->
         <el-tab-pane label="静态属性" name="only">
           <!--添加静态属性的按钮-->
-          <el-button type="primary" size="mini" :disabled="isBtnDisable">添加属性</el-button>
+          <el-button type="primary" size="mini" :disabled="isBtnDisable" @click="addDialogVisible=true">添加属性</el-button>
+          <!--静态属性的表格-->
+          <el-table :data="onlyTableData" border stripe>
+            <!--展开行-->
+            <el-table-column type="expand"></el-table-column>
+            <!--索引列-->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
+
+    <!--添加参数的对话框-->
+    <el-dialog
+      :title="'添加' + titleText"
+      :visible.sync="addDialogVisible"
+      width="50%" @close="addDialogClosed">
+      <!--添加参数的对话框-->
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="addForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addParams">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -54,9 +99,21 @@
         //被激活的页签的名称
         activeName: 'many',
         //动态参数的数据
-        manyTableData:[],
+        manyTableData: [],
         //静态属性的数据
-        onlyTableData:[]
+        onlyTableData: [],
+        //控制添加对话框的显示与隐藏
+        addDialogVisible: false,
+        //添加参数的表单数据对象
+        addForm: {
+          attr_name: ''
+        },
+        //添加表单的验证规则对象
+        addFormRules: {
+          attr_name: [{
+            required: true, message: '请输入参数名称', trigger: 'blur'
+          }]
+        }
       }
     },
     created() {
@@ -102,6 +159,27 @@
         } else {
           this.onlyTableData = res.data
         }
+      },
+      //监听添加对话框的关闭事件
+      addDialogClosed() {
+        this.$refs.addFormRef.resetFields()
+      },
+      //点击按钮，添加参数
+      addParams() {
+        this.$refs.addFormRef.validate(async valid => {
+          if (!valid) return
+          const { data: res } = await this.$http.post(`categories/${this.cateId}/attributes`, {
+            attr_name: this.addForm.attr_name,
+            attr_sel: this.activeName
+          })
+          if (res.meta.status !== 201) {
+            return this.$message.error('添加参数失败')
+          }
+
+          this.$message.success('添加参数成功')
+          this.addDialogVisible = false
+          this.getParamsData()
+        })
       }
     },
     computed: {
@@ -118,8 +196,14 @@
           return this.selectedCateKeys[2]
         }
         return null
+      },
+      // 动态计算标题的文本
+      titleText() {
+        if (this.activeName === 'many') {
+          return '动态参数'
+        }
+        return '静态属性'
       }
-      // 当前选中的三级分类的Id
 
     }
   }
