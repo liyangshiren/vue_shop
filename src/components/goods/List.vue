738867d0e6cb377021ead7731fb8720a28c32138
li <372902817@qq.com>
2019-12-20 19:16:56 +0800

    商品列表渲染完成

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -11,7 +11,7 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容" >
+          <el-input placeholder="请输入内容">
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </el-col>
@@ -19,6 +19,21 @@
           <el-button type="primary">添加商品</el-button>
         </el-col>
       </el-row>
+
+      <!--table表格区域-->
+      <el-table :data="goodslist" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column label="商品名称" prop="goods_name"></el-table-column>
+        <el-table-column label="商品价格(元)" prop="goods_price" width="95px"></el-table-column>
+        <el-table-column label="商品重量" prop="goods_weight" width="70px"></el-table-column>
+        <el-table-column label="创建时间" prop="add_time" width="140px"></el-table-column>
+        <el-table-column label="操作" width="130px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -47,7 +62,7 @@
       async getGoodsList () {
         const { data: res } = await this.$http.get(`goods`, { params: this.queryInfo })
 
-        if(res.meta.status !== 200){
+        if (res.meta.status !== 200) {
           return this.$message.error('获取商品列表失败!')
         }
 
