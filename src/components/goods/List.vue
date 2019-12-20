f22a898ed95d1d58fa89352dc5f4c65c0114cb47
li <372902817@qq.com>
2019-12-20 19:39:49 +0800

    删除商品

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -32,7 +32,8 @@
         <el-table-column label="操作" width="130px">
           <template slot-scope="scope">
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"
+                       @click="removeById(scope.row.goods_id)"></el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -83,13 +84,33 @@
         this.goodslist = res.data.goods
         this.total = res.data.total
       },
-      handleSizeChange(newSize){
+      handleSizeChange (newSize) {
         this.queryInfo.pagesize = newSize
         this.getGoodsList()
       },
-      handleCurrentChange(newPage){
+      handleCurrentChange (newPage) {
         this.queryInfo.pagenum = newPage
         this.getGoodsList()
+      },
+      async removeById (id) {
+        const confirmResult = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err => err)
+
+        if (confirmResult !== 'confirm') {
+          return this.$message.info('已经取消删除!')
+        }
+
+        const { data: res } = await this.$http.delete(`goods/${id}`)
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除失败!')
+        }
+
+        this.$message.success('删除成功!')
+        this.getGoodsList()
+
       }
     }
   }
