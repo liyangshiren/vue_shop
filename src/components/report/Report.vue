7b5d9b53a073e052b28dd38faac41404c633eed5
li <372902817@qq.com>
2019-12-23 23:44:26 +0800

    echars

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -20,36 +20,52 @@
 
   export default {
     data () {
+      //需要合并的数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis', axisPointer: {
+            type: 'cross', label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+          yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
     },
     created () {
     },
     //此时页面上的元素已经被渲染完毕了
-    mounted () {
+    async mounted () {
       //3.基于准备好的dom，初始化echarts实例
       var myChart = echarts.init(document.getElementById('main'))
 
+      const{data:res} = await this.$http.get(`reports/type/1`)
+
       //4.准备数据和配置项
-      var option = {
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-          data:['销量']
-        },
-        xAxis: {
-          data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
+      if(res.meta.status !== 200){
+        return this.$message.error('获取折线图数据失败！')
       }
 
       //5.展示数据
-      myChart.setOption(option);
+      myChart.setOption(res.data)
     },
     methods: {}
 
