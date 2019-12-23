84208f0227e5d591ebb52eb296fa4c7cd3bfee4c
li <372902817@qq.com>
2019-12-23 22:04:45 +0800

    初始化order

---
+++ src/components/order/Order.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!--面包屑导航区-->
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">订单管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!--卡片视图区-->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+    </el-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {}
+    },
+    created () {
+    },
+    methods:{}
+  }
+</script>
+
+<style lang="less"></style>
