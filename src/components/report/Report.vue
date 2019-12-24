7ebb5d4d548d5d9daa0d27d91cb5c85a066ffd66
li <372902817@qq.com>
2019-12-24 08:49:10 +0800

    echarts完成

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -17,55 +17,59 @@
 <script>
   //1.导入echarts
   import echarts from 'echarts'
+  import _ from 'lodash'
 
   export default {
-    data () {
-      //需要合并的数据
-      options: {
-        title: {
-          text: '用户来源'
-        },
-        tooltip: {
-          trigger: 'axis', axisPointer: {
-            type: 'cross', label: {
-              backgroundColor: '#E9EEF3'
+    data() {
+      return {
+        //需要合并的数据
+        options: {
+          title: {
+            text: '用户来源'
+          },
+          tooltip: {
+            trigger: 'axis', axisPointer: {
+              type: 'cross', label: {
+                backgroundColor: '#E9EEF3'
+              }
             }
-          }
-        },
-        grid: {
-          left: '3%',
+          },
+          grid: {
+            left: '3%',
             right: '4%',
             bottom: '3%',
             containLabel: true
-        },
-        xAxis: [
-          {
-            boundaryGap: false
-          }
-        ],
+          },
+          xAxis: [
+            {
+              boundaryGap: false
+            }
+          ],
           yAxis: [
-          {
-            type: 'value'
-          }
-        ]
+            {
+              type: 'value'
+            }
+          ]
+        }
       }
     },
-    created () {
+    created() {
     },
     //此时页面上的元素已经被渲染完毕了
-    async mounted () {
-      //3.基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById('main'))
+    async mounted() {
+      //3.基于准备好的dom，初始化 echarts 实例
+      const myChart = echarts.init(document.getElementById('main'))
 
-      const{data:res} = await this.$http.get(`reports/type/1`)
-
-      //4.准备数据和配置项
-      if(res.meta.status !== 200){
+      const { data: res } = await this.$http.get(`reports/type/1`)
+      if (res.meta.status !== 200) {
         return this.$message.error('获取折线图数据失败！')
       }
 
+      //4.准备数据和配置项
+      const result = _.merge(res.data, this.options)
+
       //5.展示数据
-      myChart.setOption(res.data)
+      myChart.setOption(result)
     },
     methods: {}
 
