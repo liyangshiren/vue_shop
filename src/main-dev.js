fef7f676fec21234de3e83eeea11680a98050d2a
li <372902817@qq.com>
2019-12-24 10:03:36 +0800

    自定义打包入口

---
+++ src/main-dev.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/router'
+import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+//
+import TreeTable from 'vue-table-with-tree-grid'
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor/src'
+//require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+//导入 NProgress 进度条 对应的js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+
+Vue.prototype.$http = axios
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//在 request 拦截器中展示进度条 NProgress.start()
+axios.interceptors.request.use(config => {
+  NProgress.start()
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
+//在 response 拦截器中隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
+  return config
+})
+Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
+//将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+//注册全局时间过滤器
+Vue.filter('dateFormat', function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 +'').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
