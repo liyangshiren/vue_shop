41889fd930700013cf8756ba7ef9bf88b5f0db2e
li <372902817@qq.com>
2019-12-24 09:06:26 +0800

    添加NProgress进度条效果

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -56,7 +56,7 @@
         <el-table-column type="index"></el-table-column>
         <el-table-column label="角色名称" prop="roleName"></el-table-column>
         <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
-        <el-table-column label="操作" width="300px">
+        <el-table-column label="操作" width="350px">
           <template slot-scope="scope">
             <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
             <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
