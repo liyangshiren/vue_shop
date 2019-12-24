489573cd7fce1a7fc3ed10e98b18491eb9406f42
li <372902817@qq.com>
2019-12-24 15:01:22 +0800

    修复下拉显示问题

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -33,7 +33,7 @@
             <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作" width="180px">
+        <el-table-column label="操作" width="200px">
           <template slot-scope="scope">
             <!--修改按钮-->
             <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)"></el-button>
